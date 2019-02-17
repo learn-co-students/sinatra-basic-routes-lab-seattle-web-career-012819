bc0a2cc6c45b13e1f7f51c30d7fe90629fff5cb1
github username <github email address>
2019-02-17 00:51:51 -0800

    First commit.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
+# I wonder why string interpolation fails...
+# Could it be... sanitation?
+
 class App < Sinatra::Base
+  get '/' do
+    "Hello, and welcome to my lab.<br>
+    Please navigate to /name, /hometown, or /favorite-song to test my routes.<br>
+    Thank you for visiting!"
+  end
+
+  get '/name' do
+    "My name is Uninterpolated Name"
+  end
+
+  get '/hometown' do
+    "My hometown is Uninterpolatedville"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Uninterpolated Rhapsody"
+  end
 end
