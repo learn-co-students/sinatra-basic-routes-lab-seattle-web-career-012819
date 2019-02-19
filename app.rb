263605087a05558aeeec6ee7810b2cb806e8a91a
Madison Stankevich <madison.stankevich@flatironschool.com>
2019-02-18 20:29:21 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get "/" do
+    "Hello World!"
+  end
+
+  get "/name" do
+    "My name is Madison"
+  end
+
+  get "/hometown" do
+    "My hometown is Nashville"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Mr. Brightside"
+  end
+
 end
