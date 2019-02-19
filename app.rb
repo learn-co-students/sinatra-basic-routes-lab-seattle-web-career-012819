1f34f0c52bd99e1910bfc6518ad6b30e2ee5a6cd
Katie Carrel <katiecarrel@comcast.net>
2019-02-19 11:45:47 -0800

    finished

--- app.rb
+++ app.rb
@@ -1,4 +1,22 @@
 require_relative 'config/environment'
-
+require 'sinatra'
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Katie"
+  end
+
+  get '/hometown' do
+    "My hometown is Federal Way"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Sunflower"
+  end 
+
+
 end
