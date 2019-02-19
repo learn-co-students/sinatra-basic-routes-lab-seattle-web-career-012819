a16b03e78a29e597dc2c53b98e1945a3b4f735c8
Nat-Reid <nathanielreid@gmail.com>
2019-02-19 12:29:41 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    "Hello, World!"
+  end
+  get '/name' do
+    "My name is Nat."
+  end
+  get '/hometown' do
+    "My hometown is Providence, RI."
+  end
+  get '/favorite-song' do
+    "My favorite song is twist and shout."
+  end
 end
