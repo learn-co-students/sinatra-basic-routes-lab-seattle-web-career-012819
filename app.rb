2f4c5e32125fbe5a9773b2e945fa764e72fb24fc
omfgtora <spaminol@gmail.com>
2019-02-19 09:05:50 -0800

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
-require_relative 'config/environment'
+require_relative "config/environment"
 
 class App < Sinatra::Base
+  get "/" do
+    "Something"
+  end
+
+  get "/name" do
+    "My name is Ethan"
+  end
+
+  get "/hometown" do
+    "My hometown is Earth"
+  end
+
+  get "/favorite-song" do
+    "My favorite song is Push it - Salt n Peppa"
+  end
 end
